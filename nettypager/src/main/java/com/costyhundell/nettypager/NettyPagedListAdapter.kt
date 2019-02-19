a2e3bf27d3196878aa6d37dd65aee45764b6a073
Thomas Cundell <thomascundell@MacBook-Pro-4.local>
2019-02-19 22:31:08 +0100

    small refactor

--- nettypager/src/main/java/com/costyhundell/nettypager/NettyPagedListAdapter.kt
+++ nettypager/src/main/java/com/costyhundell/nettypager/NettyPagedListAdapter.kt
@@ -6,12 +6,9 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class NettyPagedListAdapter<U : RecyclerView.ViewHolder>(DIFF_CALLBACK: DiffUtil.ItemCallback<NettyItem>): PagedListAdapter<NettyItem, U>(DIFF_CALLBACK) {
-    var item: NettyItem? = null
     abstract override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): U
 
-    override fun onBindViewHolder(holder: U, position: Int) {
-        item = getItem(position)
-    }
+    abstract override fun onBindViewHolder(holder: U, position: Int)
 
     override fun getItemViewType(position: Int): Int = getItem(position)!!.getItemViewType()
 }
